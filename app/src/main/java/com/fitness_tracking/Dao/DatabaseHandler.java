a6d713daddf3d611249aba6a9ca44ca0d7ac3f65
mouad <mouadbenthami.mb@gmail.com>
2024-02-07 18:49:58 +0100

    dao

---
+++ app/src/main/java/com/fitness_tracking/Dao/DatabaseHandler.java
@@ -0,0 +1,26 @@
+package com.fitness_tracking.Dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    public DatabaseHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1)
+    {
+
+    }
+}

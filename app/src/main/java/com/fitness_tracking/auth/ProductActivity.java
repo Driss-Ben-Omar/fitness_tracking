ed17a10eae8274ee139481acbeeab1d6b45984de
Driss Benomar <benomard999@gmail.com>
2024-02-08 23:35:23 +0100

    fixing the error

--- app/src/main/java/com/fitness_tracking/auth/ProductActivity.java
+++ app/src/main/java/com/fitness_tracking/auth/ProductActivity.java
@@ -1,7 +1,9 @@
 package com.fitness_tracking.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,7 @@ import com.fitness_tracking.Dao.DatabaseHandler;
 import com.fitness_tracking.Dao.ProductAdapter;
 import com.fitness_tracking.R;
 import com.fitness_tracking.entities.Produit;
+import com.fitness_tracking.pages.Home;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,20 +19,22 @@ import java.util.List;
 public class ProductActivity extends AppCompatActivity {
     ProductAdapter listAdapter;
     List<Produit> dataArrayList = new ArrayList<>();
-    DatabaseHandler databaseHandler;
+    DatabaseHandler databaseHandler=new DatabaseHandler(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.produit); // Replace 'your_layout_file_name' with the actual XML layout file name
+        setContentView(R.layout.produit);
 
         ListView listView = findViewById(R.id.listview);
 
         Long id = SessionManager.getInstance().getCurrentUser().getId();
+
+
         dataArrayList = databaseHandler.getAllProduitsForUser(id);
-        listAdapter = new ProductAdapter(ProductActivity.this, dataArrayList);
+
+        listAdapter = new ProductAdapter(getApplicationContext(), dataArrayList);
 
         listView.setAdapter(listAdapter);
-        listView.setClickable(true);
     }
 }
